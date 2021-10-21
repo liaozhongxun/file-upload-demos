de0bcda5034814f96df906d8b6ae983345f35c4e
liaozhongxun <869664233@qq.com>
2021-10-21 14:57:06 +0800

    updata

--- single-file-upload/single-file-server.js
+++ single-file-upload/single-file-server.js
@@ -12,41 +12,41 @@ const RESOURCE_URL = `http://localhost:${PORT}`;
 const UPLOAD_DIR = path.join(__dirname, "/public/upload");
 
 const storage = multer.diskStorage({
-  destination: async function (req, file, cb) {
-    // 设置文件的存储目录
-    cb(null, UPLOAD_DIR);
-  },
-  filename: function (req, file, cb) {
-    // 设置文件名
-    cb(null, `${file.originalname}`);
-  },
+    destination: async function (req, file, cb) {
+        // 设置文件的存储目录
+        cb(null, UPLOAD_DIR);
+    },
+    filename: function (req, file, cb) {
+        // 设置文件名
+        cb(null, `${file.originalname}`);
+    },
 });
 
 const multerUpload = multer({ storage });
 
 router.get("/", async (ctx) => {
-  ctx.body = "文件上传服务（by 阿宝哥）";
+    ctx.body = "文件上传服务（by 阿宝哥）";
 });
 
 router.post(
-  "/upload/single",
-  async (ctx, next) => {
-    try {
-      await next();
-      ctx.body = {
-        code: 1,
-        msg: "文件上传成功",
-        url: `${RESOURCE_URL}/${ctx.file.originalname}`,
-      };
-    } catch (error) {
-      console.dir(error);
-      ctx.body = {
-        code: 0,
-        msg: "文件上传失败",
-      };
-    }
-  },
-  multerUpload.single("file")
+    "/upload/single",
+    async (ctx, next) => {
+        try {
+            await next();
+            ctx.body = {
+                code: 1,
+                msg: "文件上传成功",
+                url: `${RESOURCE_URL}/${ctx.file.originalname}`,
+            };
+        } catch (error) {
+            console.dir(error);
+            ctx.body = {
+                code: 0,
+                msg: "文件上传失败",
+            };
+        }
+    },
+    multerUpload.single("file")
 );
 
 // 注册中间件
@@ -55,5 +55,5 @@ app.use(serve(UPLOAD_DIR));
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
-  console.log(`应用已经启动：http://localhost:${PORT}/`);
+    console.log(`应用已经启动：http://localhost:${PORT}/`);
 });
